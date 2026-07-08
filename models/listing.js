0ac0c59d7fb8c0815d4335f85b2e2bd5b424669f
Sumit <sumitkumar787se@gmail.com>
2026-07-08 15:13:40 +0530

    update

--- models/listing.js
+++ models/listing.js
@@ -6,22 +6,21 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-
     description: String,
-    url: {
-    type: String,
-    default: "https://images.unsplash.com/photo-1625505826533-c5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",
-
-    set: (v) => 
-        v === ""
-    ? "https://images.unsplash.com/photo-1625505826533-c5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
-    : v,
+    image: {
+        filename: String,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (v) =>
+                v === ""
+                    ? "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    : v,
+        },
     },
-
     price: Number,
     location: String,
     country: String,
-
 });
 
 const Listing = mongoose.model("Listing", listingSchema);

f950afcdfd2dacfa2142b2855ccb254d28031057
Sumit <sumitkumar787se@gmail.com>
2026-07-16 11:57:11 +0530

    updated new.ejs,index.ejs and app.js codes

--- init/index.js
+++ init/index.js
@@ -2,26 +2,25 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = ('mongodb://127.0.0.1:27017/wanderlust');
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then(() => {
+  .then(() => {
     console.log("connected to DB");
-})
-
-.catch((err) =>{
-    console.log(error);
-});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
-
 const initDB = async () => {
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({ ...obj }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
 };
 
 initDB();
